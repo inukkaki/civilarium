54f51838001356ba4ca022e35719718a04898681
inukkaki <kgn.gkrk.rstb@gmail.com>
2026-09-15 21:25:24 +0900

    Create tests/fluid

---
+++ tests/fluid.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+#include <SDL2/SDL.h>
+
+#include "system/launch.h"
+#include "time/framerate.h"
+
+#if (defined(__WIN32) || defined(__WIN64))
+    #include "system/winfunc.h"
+#endif
+
+namespace impl {
+
+namespace launch = civilarium::system::launch;
+namespace framerate = civilarium::time::framerate;
+
+}  // namespace impl
+
+inline constexpr float kWindowScale = 4.0f;
+
+inline constexpr int kFieldWidth = 256;
+inline constexpr int kFieldHeight = 128;
+
+int main(int argc, char* argv[])
+{
+    // Resolve the blurriness of the screen
+    #if (defined(__WIN32) || defined(__WIN64))
+        civilarium::system::winfunc::ResolveBlurriness();
+    #endif
+
+    // Create the main window
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+
+    int window_width = static_cast<int>(kWindowScale*kFieldWidth);
+    int window_height = static_cast<int>(kWindowScale*kFieldHeight);
+    bool initializes_gui = impl::launch::InitGui(
+        window, renderer, window_width, window_height, "fluid",
+        kWindowScale);
+
+    // Main routine
+    impl::framerate::FrameRateAdjuster fra(60);
+    impl::framerate::FrameRateMeasurer frm;
+    double measured_frame_rate = 0.0;
+
+    if (initializes_gui) {
+        // DEBUG
+        int count = 0;
+        while (true) {
+            if (count > 5) {
+                break;
+            }
+
+            // Frame rate
+            if (frm.MeasureFrameRate(measured_frame_rate)) {
+                std::cout << "FPS: " << measured_frame_rate << std::endl;
+                ++count;
+            }
+            fra.Adjust();
+        }
+    }
+
+    // Close the GUI
+    impl::launch::CloseGui(window, renderer);
+
+    return 0;
+}
